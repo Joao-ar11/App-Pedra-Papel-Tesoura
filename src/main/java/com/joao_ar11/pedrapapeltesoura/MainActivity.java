274baca12ddd487ac0ae3e563b7955b1d54f1dd4
João_A.11 <jarlpereira29@gmail.com>
2025-01-06 16:50:09 -0300

    Adiciona eventos de clique para os botoes

--- src/main/java/com/joao_ar11/pedrapapeltesoura/MainActivity.java
+++ src/main/java/com/joao_ar11/pedrapapeltesoura/MainActivity.java
@@ -1,8 +1,12 @@
 package com.joao_ar11.pedrapapeltesoura;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.activity.ComponentActivity;
+
+import java.util.Random;
 
 public class MainActivity extends ComponentActivity {
 
@@ -10,5 +14,20 @@ public class MainActivity extends ComponentActivity {
     protected void onCreate(Bundle savedInstances) {
         super.onCreate(savedInstances);
         setContentView(R.layout.activity_main);
+    }
+
+    public void selecionarPedra(View view) {
+        verificarGanhador("Pedra");
+    }
+
+    public void selecionarPapel(View view) {
+        verificarGanhador("Papel");
+    }
+
+    public void selecionarTesoura(View view) {
+        verificarGanhador("Tesoura");
+    }
+
+    public void verificarGanhador(String escolhaUsuario) {
     }
 }
